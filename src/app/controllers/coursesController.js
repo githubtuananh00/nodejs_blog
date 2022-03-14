b99b6713c9e2cda4f12f0364b9dde0358c67e337
TuanAnh <laituananh22k@gmail.com>
2022-03-14 16:53:56 +0700

    Create document mongodb

--- src/app/controllers/coursesController.js
+++ src/app/controllers/coursesController.js
@@ -2,13 +2,26 @@ const Course = require('../modules/Courses')
 const { mongooseToObject } = require('../../util/toolMongoose')
 
 class CourseController {
-	// [GET] /course/:slug
+	// [GET] /courses/:slug
 	show(req, res, next) {
 		Course.findOne({ slug: req.params.slug })
 			.then((course) => {
 				res.render('courses/show', { course: mongooseToObject(course) })
 			})
 			.catch(next)
+	}
+	// [GET] /courses/create
+	create(req, res, next) {
+		res.render('courses/create')
+	}
+
+	// [POST] /courses/store
+	store(req, res, next) {
+		const course = new Course(req.body)
+		course.save()
+		res.send('ok')
+			.then(() => res.redirect('/'))
+			.catch((err) => {})
 	}
 }
 
--- src/app/modules/Courses.js
+++ src/app/modules/Courses.js
@@ -1,12 +1,25 @@
 const { mongoose } = require('mongoose')
+const slug = require('mongoose-slug-generator')
+
+mongoose.plugin(slug)
 
 const Schema = mongoose.Schema
-const Course = new Schema({
-	name: { type: String, default: 'Blog_Name', maxLength: 255 },
-	description: { type: String, default: 'Description', maxLength: 600 },
-	img: { type: String, default: '' },
-	createdAt: { type: Date, default: Date.now },
-	updatedAt: { type: Date, default: Date.now },
-})
+const Course = new Schema(
+	{
+		name: {
+			type: String,
+			default: 'Blog_Name',
+			maxLength: 255,
+			required: true,
+		},
+		description: { type: String, default: 'Description', maxLength: 600 },
+		img: { type: String, default: '' },
+		videoId: { type: String, required: true },
+		slug: { type: String, slug: 'name', unique: true },
+	},
+	{
+		timestamps: true,
+	}
+)
 
 module.exports = mongoose.model('Course', Course)
